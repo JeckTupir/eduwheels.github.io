72c3f4151d92083f0488fe297d6ca20e2d5714c1
LAB22_USER ON WS37 <L22X07W37@citulab.net>
2025-02-10 09:29:46 +0800

    backend sh

---
+++ backend/eduwheels/src/test/java/edu/cit/eduwheels/BcdEduWheelsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.eduwheels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcdEduWheelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
