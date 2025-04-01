75e8b86ceb3d708735873d30e6e3eb003a863774
Xyrill Dereck Cañete <xdcanet@gmail.com>
2025-04-02 01:02:37 +0800

    Added User APIs and Swagger

---
+++ EduWheels/src/main/java/com/example/eduwheels/EduWheelsApplication.java
@@ -0,0 +1,13 @@
+package com.example.eduwheels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduWheelsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduWheelsApplication.class, args);
+    }
+
+}
